e4b86d8b95a73816a688733935bc5c614173372e
Nick Evers <evers.nick@gmail.com>
2015-08-21 09:57:17 +0200

    Moved wifi code to wifi.{c,h}

--- src/main.c
+++ src/main.c
@@ -1,27 +1,14 @@
 #include "log.h"
 #include "http.h"
+#include "wifi.h"
 #include "user_config.h"
 #include "osapi.h"
 
-void wifi_callback(System_Event_t *event);
 void handle_adc(HttpRequest* request, struct espconn* connection);
 
 void ICACHE_FLASH_ATTR user_init() {
     log_init();
-    println("[wifi] init");
-    char ssid[32] = SSID;
-    char pwd[64] = PWD;
-    struct station_config config;
-
-    wifi_set_opmode(STATION_MODE);
-    wifi_station_set_hostname(HOST);
-
-    os_memcpy(&config.ssid, ssid, 32);
-    os_memcpy(&config.password, pwd, 64);
-
-    wifi_station_set_config(&config);
-    wifi_set_event_handler_cb(wifi_callback);
-
+    wifi_init();
     http_init();
     http_add_request_handler(&handle_adc);
 }
@@ -33,21 +20,3 @@ void handle_adc(HttpRequest* request, struct espconn* connection) {
     http_send(connection, buffer);
 }
 
-void wifi_callback(System_Event_t *evt) {
-    switch(evt->event) {
-        case EVENT_STAMODE_CONNECTED:
-            println("[wifi] connected");
-            break;
-
-         case EVENT_STAMODE_DISCONNECTED:
-            println("[wifi] disconnected");
-            break;
-
-         case EVENT_STAMODE_GOT_IP:
-            print("[wifi] got ip: ");
-            char ip[16];
-            os_sprintf(ip, IPSTR, IP2STR(&evt->event_info.got_ip.ip));
-            println(ip);
-            break;
-    }
-}
